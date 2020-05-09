ca8a0e7e405e635509b1c22e828836be7b3efddb
ES Ivy <esivy@phytapublishing.com>
2020-05-09 18:01:31 -0500

    Upload images

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1589001067;
+// updated_on=1589001067;
+exit(0);
+?>
+1589001067_9168:"Notice: 172.21.0.1; User authentication succeeded: esivy"
---
+++ wp-content/uploads/sucuri/sucuri-settings.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"sucuriscan_selfhosting_monitor":"disabled","sucuriscan_selfhosting_fpath":"","sucuriscan_api_service":"enabled","sucuriscan_notify_to":"esivy@phytapublishing.com","sucuriscan_notify_success_login":"disabled","sucuriscan_addr_header":"HTTP_X_SUCURI_CLIENTIP","sucuriscan_lastlogin_redirection":"enabled","sucuriscan_revproxy":"disabled"}
